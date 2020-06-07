c9747da7b5e01fb694aa928ed4ca32eb0ca19455
mcoons <mcoons67@gmail.com>
2020-06-07 01:02:55 -0600

    fixed put

--- server.js
+++ server.js
@@ -125,12 +125,30 @@ app.get("/api/contacts/:id", function (req, res) {
         _id: new ObjectID(req.params.id)
     }, function (err, doc) {
         if (err) {
-            handleError(res, err.message, "Failed to get contact");
+            handleError(res, err.message, "Failed to get contact",400);
         } else {
             res.status(200).json(doc);
         }
     });
 });
+
+// app.put("/api/contacts/:id", function (req, res) {
+//     var updateDoc = req.body;
+//     delete updateDoc._id;
+
+//     db.collection(CONTACTS_COLLECTION).updateOne({
+//         _id: new ObjectID(req.params.id)
+//     }, updateDoc, function (err, doc) {
+//         if (err) {
+//             handleError(res, err.message, "Failed to update contact");
+//         } else {
+//             updateDoc._id = req.params.id;
+//             res.status(200).json(updateDoc);
+//         }
+//     });
+// });
+
+
 
 app.put("/api/contacts/:id", function (req, res) {
     var updateDoc = req.body;
@@ -138,7 +156,7 @@ app.put("/api/contacts/:id", function (req, res) {
 
     db.collection(CONTACTS_COLLECTION).updateOne({
         _id: new ObjectID(req.params.id)
-    }, updateDoc, function (err, doc) {
+    }, {$set: updateDoc }, function (err, doc) {
         if (err) {
             handleError(res, err.message, "Failed to update contact");
         } else {
@@ -147,6 +165,7 @@ app.put("/api/contacts/:id", function (req, res) {
         }
     });
 });
+
 
 app.delete("/api/contacts/:id", function (req, res) {
     db.collection(CONTACTS_COLLECTION).deleteOne({
